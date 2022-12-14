683f5c3f66a2e423e4e48146fe6a2f9e875e529d
DimaZhuravlov <dzhv26@gmail.com>
2022-12-14 11:15:46 +0200

    updates

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import MainBlock from "../MainBlock/MainBlock";
 import SearchBlock from "../SearchBlock/SearchBlock";
 import LessonPage from "../LessonPage/LessonPage";
 import { fetchLesson } from "../fetchLesson/fetchLesson";
 import MyError from "../error/error";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
 	{
 		path: "/",
 		element: <MainBlock />,
