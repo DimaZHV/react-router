798003fc4dac584d3ef67cf530f2d8b0cb683a84
DimaZhuravlov <dzhv26@gmail.com>
2022-12-15 23:37:52 +0200

    fixed bugs

--- src/SearchBlock/SearchBlock.tsx
+++ src/SearchBlock/SearchBlock.tsx
@@ -9,13 +9,14 @@ function SearchBlock() {
 	const [SEARCH_KEYWORD, setSEARCH] = useState("");
 	const [lessonsList, setLessonsList] = useState([]);
 	const [error, setError] = useState<boolean>(false)
+	const [empty, setEmpty] = useState<boolean>(false)
 	const lessonFinder = (
 		e: React.FormEvent<HTMLButtonElement> | React.FormEvent<HTMLFormElement>
 	) => {
 		e.preventDefault();
 		fetch(`https://react-course-api.azurewebsites.net/lesson/${SEARCH_KEYWORD}`)
 			.then((x) => { if (x.ok) { setError(false); return x.json() } if (!x.ok) { setError(true); return x.json() } })
-			.then((x) => { setLessonsList(x); setError(false) })
+			.then((x) => { if (x.length === 0) { setEmpty(true); setLessonsList(x) } else { setLessonsList(x); setError(false); setEmpty(false) } })
 			.catch((error) => console.log(error));
 	};
 
@@ -23,7 +24,7 @@ function SearchBlock() {
 
 		<div className="search_container">
 			<InputBlock lessonFinder={lessonFinder} setSEARCH={setSEARCH} />
-			{lessonsList.length === 0 && !error ? <div className="error">Please, write correct word</div> : null}
+			{empty && !error ? <div className="error">Please, write correct word</div> : null}
 			{error ? <div className="error" >Something went wrong, click button "Search" again</div> : <LessonList lessonsList={lessonsList} />}
 		</div>
 	);
